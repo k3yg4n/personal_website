e2670e2d9e8d7d7931f56cefc62cbfc4be1f14e7
Keegan Liu <keeganliu5678@gmail.com>
2022-10-14 01:21:56 -0400

    center object, background, and stars complete

--- personal_website/main.js
+++ personal_website/main.js
@@ -1,23 +1,73 @@
 import './style.css'
-import javascriptLogo from './javascript.svg'
-import { setupCounter } from './counter.js'
+import * as THREE from 'three'; // import threejs library
+import {OrbitControls} from 'three/examples/jsm/controls/OrbitControls'
 
-document.querySelector('#app').innerHTML = `
-  <div>
-    <a href="https://vitejs.dev" target="_blank">
-      <img src="/vite.svg" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://developer.mozilla.org/en-US/docs/Web/JavaScript" target="_blank">
-      <img src="${javascriptLogo}" class="logo vanilla" alt="JavaScript logo" />
-    </a>
-    <h1>Hello Vite!</h1>
-    <div class="card">
-      <button id="counter" type="button"></button>
-    </div>
-    <p class="read-the-docs">
-      Click on the Vite logo to learn more
-    </p>
-  </div>
-`
+// Initialize scene (container) that holds objects, cameras, lights
+const scene = new THREE.Scene(); 
+// Initialize camera to provide point of view
+const camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
+// Initializes renderer to render the graphics of the scene
+const renderer = new THREE.WebGLRenderer({
+  canvas: document.querySelector('#bg'),
+});
 
-setupCounter(document.querySelector('#counter'))
+renderer.setPixelRatio(window.devicePixelRatio);
+renderer.setSize(window.innerWidth, window.innerHeight);
+camera.position.setZ(30);
+
+// Initialize lights and associated helpers to view object
+const pointLight = new THREE.PointLight(0xffffff);
+pointLight.position.set(0,0,0);
+scene.add(pointLight);
+
+const lightHelper = new THREE.PointLightHelper(pointLight);
+const gridHelper = new THREE.GridHelper(200,50);
+scene.add(lightHelper, gridHelper);
+
+// Ambient light will brighten entire background
+const ambientLight = new THREE.AmbientLight(0xfffff);
+scene.add(ambientLight);
+
+// Instantiate orbit controls object
+const controls = new OrbitControls(camera, renderer.domElement);
+
+
+// Create 3-D objects to display
+// Torus Knot
+const geometry = new THREE.TorusKnotGeometry( 10, 3, 64, 8, 2, 3 );
+const material = new THREE.MeshStandardMaterial( { color: 0xF030B0, wireframe: true } );
+const torusKnot = new THREE.Mesh(geometry, material);
+scene.add(torusKnot);
+
+// Stars
+function addStar() {
+  const geometry = new THREE.SphereGeometry(0.25, 24, 24);
+  const material = new THREE.MeshStandardMaterial( { color: 0xFFFFFF, wireframe: false } );
+  const star= new THREE.Mesh( geometry, material );
+
+  const [x, y, z] = Array(3).fill().map(() => THREE.MathUtils.randFloatSpread(100));
+
+  star.position.set(x,y,z);
+  scene.add(star);
+}
+
+Array(200).fill().forEach(addStar);
+
+// Initialize background
+const spaceTexture = new THREE.TextureLoader().load('space.jpg');
+scene.background = spaceTexture;
+
+// Infinite loop to continue rerendering scene
+function animate() {
+  requestAnimationFrame(animate);
+
+  torusKnot.rotation.x += 0.001;
+  torusKnot.rotation.y += 0.002;
+  torusKnot.rotation.z += 0.001;
+
+  controls.update();
+
+  renderer.render(scene, camera);
+}
+
+animate();
